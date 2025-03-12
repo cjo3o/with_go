8fd29ba0379ab7a99cd57156c67b780324d2dfc9
cjo3o <cjy55973743@gmail.com>
2025-03-12 18:12:03 +0900

    배송예약, 예약확인 페이지 완료

--- check.js
+++ check.js
@@ -12,123 +12,128 @@ document.addEventListener('DOMContentLoaded', function () {
     const $delivery_table = document.querySelector('.delivery_table');
     const $click_infor = document.querySelector('.click_infor');
 
-    $keep_btn.addEventListener('click', function () {
-        $keep_btn.classList.add('active');
-        $keep_table.classList.add('up');
-        $delivery_btn.classList.remove('active');
-        $delivery_table.classList.remove('up');
-        checkKeepTableVisibility(); // Keep 테이블의 가시성 확인
-    });
 
-    $delivery_btn.addEventListener('click', function () {
-        $keep_btn.classList.remove('active');
-        $keep_table.classList.remove('up');
-        $delivery_btn.classList.add('active');
-        $delivery_table.classList.add('up');
-        checkDeliveryTableVisibility(); // Delivery 테이블의 가시성 확인
-    });
 
-    // close 버튼 클릭 이벤트 리스너
-    function addCloseEvent() {
-        const $close = document.querySelector('.close');
-        if ($close) {
-            $close.addEventListener('click', function () {
-                $check_detail.classList.remove('fade_in');
-                $check_detail_contents.classList.remove('slide_up');
-                console.log('클릭');
-            });
-        }
-    }
+    // $keep_btn.addEventListener('click', function () {
+    //     $keep_btn.classList.add('active');
+    //     $keep_table.classList.add('up');
+    //     $delivery_btn.classList.remove('active');
+    //     $delivery_table.classList.remove('up');
+    //     checkKeepTableVisibility(); // Keep 테이블의 가시성 확인
+    // });
+    //
+    // $delivery_btn.addEventListener('click', function () {
+    //     $keep_btn.classList.remove('active');
+    //     $keep_table.classList.remove('up');
+    //     $delivery_btn.classList.add('active');
+    //     $delivery_table.classList.add('up');
+    //     checkDeliveryTableVisibility(); // Delivery 테이블의 가시성 확인
+    // });
+    //
+    // // close 버튼 클릭 이벤트 리스너
+    // function addCloseEvent() {
+    //     const $close = document.querySelector('.close');
+    //     if ($close) {
+    //         $close.addEventListener('click', function () {
+    //             $check_detail.classList.remove('fade_in');
+    //             $check_detail_contents.classList.remove('slide_up');
+    //             console.log('클릭');
+    //         });
+    //     }
+    // }
+    //
+    // // openDetail 함수
+    // window.openDetail = function (td) {
+    //     const row = td.parentElement; // 클릭한 td의 부모 tr 요소
+    //     const location = row.cells[2].innerText; // 보관 장소
+    //     const date = row.cells[1].innerText; // 보관 일자
+    //     const number = row.cells[0].innerText; // 수화물 번호
+    //     const price = row.cells[3].innerText; // 가격
+    //     const stat = row.cells[4].innerText; // 상태
+    //     $check_detail_contents.innerHTML = `
+    //         <span class="close">&times;</span>
+    //         <h1>조회 상세 정보</h1>
+    //         <span>${stat}</span>
+    //         <span style="font-size: 1.3rem;">보관 장소 : ${location}</span>
+    //         <span>보관 일자 : ${date}</span>
+    //         <span>수화물 번호 : ${number}</span>
+    //         <ul>가격
+    //             <li>소형 1개 1000원</li>
+    //             <li>중형 1개 2000원</li>
+    //             <li>대형 1개 3000원</li>
+    //         </ul>
+    //         <hr>
+    //         <p>총합<span>${price}원</span></p>
+    //     `;
+    //     $check_detail.classList.add('fade_in');
+    //     $check_detail_contents.classList.add('slide_up');
+    //
+    //     // close 버튼 이벤트 리스너 추가
+    //     addCloseEvent();
+    // };
+    //
+    // function checkSearch() {
+    //     const searchValue = $search_checkBox.value.toLowerCase(); // 검색 입력값
+    //     const rows = document.querySelectorAll('table tbody tr'); // 테이블의 모든 행 선택
+    //     let hasMatchingRows = false;
+    //
+    //     rows.forEach(row => {
+    //         const numberCell = row.cells[0]; // 수화물 번호가 있는 셀 선택
+    //
+    //         // 수화물 번호가 검색값을 포함하는지 확인
+    //         if (numberCell.textContent.toLowerCase().includes(searchValue)) {
+    //             row.style.display = ''; // 행을 표시
+    //             hasMatchingRows = true; // 검색된 값이 있는 행이 존재함
+    //         } else {
+    //             row.style.display = 'none'; // 행을 숨김
+    //         }
+    //     });
+    //
+    //     // 검색된 값이 있는 행이 없으면 테이블 숨기기
+    //     if (!hasMatchingRows) {
+    //         $view_table_container.style.display = 'none'; // 테이블 숨기기
+    //         $alert.style.display = "block"; // 경고 메시지 표시
+    //     } else {
+    //         $view_table_container.style.display = ''; // 테이블 표시
+    //         $alert.classList.remove("on");
+    //     }
+    //
+    //     // 검색창 관련 로직
+    //     $search_check.style.display = 'none';
+    //
+    //     checkKeepTableVisibility();
+    //     checkDeliveryTableVisibility();
+    // }
+    //
+    // function checkKeepTableVisibility() {
+    //     const rows = $keep_table.querySelectorAll('tbody tr'); // Keep 테이블의 모든 행 선택
+    //     let hasRows = Array.from(rows).some(row => row.style.display !== 'none'); // 표시된 행이 있는지 확인
+    //
+    //     if (!hasRows) {
+    //         $keep_table.classList.remove("up");// Keep 테이블 숨기기
+    //         $alert.style.display = "block";
+    //     } else {
+    //         $keep_table.style.display = '';// Keep 테이블 표시
+    //         $alert.style.display = 'none';
+    //     }
+    // }
+    //
+    // function checkDeliveryTableVisibility() {
+    //     const rows = $delivery_table.querySelectorAll('tbody tr'); // Delivery 테이블의 모든 행 선택
+    //     let hasRows = Array.from(rows).some(row => row.style.display !== 'none'); // 표시된 행이 있는지 확인
+    //
+    //     if (!hasRows) {
+    //         $delivery_table.classList.remove("up"); // Delivery 테이블 숨기기
+    //         $alert.style.display = "block";
+    //     } else {
+    //         $delivery_table.style.display = ''; // Delivery 테이블 표시
+    //         $alert.style.display = 'none';
+    //     }
+    // }
 
-    // openDetail 함수
-    window.openDetail = function (td) {
-        const row = td.parentElement; // 클릭한 td의 부모 tr 요소
-        const location = row.cells[2].innerText; // 보관 장소
-        const date = row.cells[1].innerText; // 보관 일자
-        const number = row.cells[0].innerText; // 수화물 번호
-        const price = row.cells[3].innerText; // 가격
-        const stat = row.cells[4].innerText; // 상태
-        $check_detail_contents.innerHTML = `
-            <span class="close">&times;</span>
-            <h1>조회 상세 정보</h1>
-            <span>${stat}</span>
-            <span style="font-size: 1.3rem;">보관 장소 : ${location}</span>
-            <span>보관 일자 : ${date}</span>
-            <span>수화물 번호 : ${number}</span>
-            <ul>가격
-                <li>소형 1개 1000원</li>
-                <li>중형 1개 2000원</li>
-                <li>대형 1개 3000원</li>
-            </ul>
-            <hr>
-            <p>총합<span>${price}원</span></p>
-        `;
-        $check_detail.classList.add('fade_in');
-        $check_detail_contents.classList.add('slide_up');
+    
 
-        // close 버튼 이벤트 리스너 추가
-        addCloseEvent();
-    };
-
-    function checkSearch() {
-        const searchValue = $search_checkBox.value.toLowerCase(); // 검색 입력값
-        const rows = document.querySelectorAll('table tbody tr'); // 테이블의 모든 행 선택
-        let hasMatchingRows = false;
-
-        rows.forEach(row => {
-            const numberCell = row.cells[0]; // 수화물 번호가 있는 셀 선택
-
-            // 수화물 번호가 검색값을 포함하는지 확인
-            if (numberCell.textContent.toLowerCase().includes(searchValue)) {
-                row.style.display = ''; // 행을 표시
-                hasMatchingRows = true; // 검색된 값이 있는 행이 존재함
-            } else {
-                row.style.display = 'none'; // 행을 숨김
-            }
-        });
-
-        // 검색된 값이 있는 행이 없으면 테이블 숨기기
-        if (!hasMatchingRows) {
-            $view_table_container.style.display = 'none'; // 테이블 숨기기
-            $alert.style.display = "block"; // 경고 메시지 표시
-        } else {
-            $view_table_container.style.display = ''; // 테이블 표시
-            $alert.classList.remove("on");
-        }
-
-        // 검색창 관련 로직
-        $search_check.style.display = 'none';
-
-        checkKeepTableVisibility();
-        checkDeliveryTableVisibility();
-    }
-
-    function checkKeepTableVisibility() {
-        const rows = $keep_table.querySelectorAll('tbody tr'); // Keep 테이블의 모든 행 선택
-        let hasRows = Array.from(rows).some(row => row.style.display !== 'none'); // 표시된 행이 있는지 확인
-
-        if (!hasRows) {
-            $keep_table.classList.remove("up");// Keep 테이블 숨기기
-            $alert.style.display = "block";
-        } else {
-            $keep_table.style.display = '';// Keep 테이블 표시
-            $alert.style.display = 'none';
-        }
-    }
-
-    function checkDeliveryTableVisibility() {
-        const rows = $delivery_table.querySelectorAll('tbody tr'); // Delivery 테이블의 모든 행 선택
-        let hasRows = Array.from(rows).some(row => row.style.display !== 'none'); // 표시된 행이 있는지 확인
-
-        if (!hasRows) {
-            $delivery_table.classList.remove("up"); // Delivery 테이블 숨기기
-            $alert.style.display = "block";
-        } else {
-            $delivery_table.style.display = ''; // Delivery 테이블 표시
-            $alert.style.display = 'none';
-        }
-    }
-
-    // 검색 입력 필드에 이벤트 리스너 추가
-    $search_check_btn.addEventListener('click', checkSearch);
 });
+function aa() {
+    console.log('조회');
+}
--- delivery_reserve.js
+++ delivery_reserve.js
@@ -15,6 +15,18 @@ const small = document.querySelector('#small');
 const medium = document.querySelector('#medium');
 const large = document.querySelector('#large');
 const agree = document.querySelector('#agree');
+const $check_date = document.querySelector('#check_date');
+const $check_start = document.querySelector('#check_start');
+const $check_arrive = document.querySelector('#check_arrive');
+const $check_detail_adr = document.querySelector('#check_detail_adr');
+const $check_name = document.querySelector('#check_name');
+const $check_phone = document.querySelector('#check_phone');
+const $keep_reservation_contents = document.querySelector('.keep_reservation_contents');
+const $keep_reservation_check_contents = document.querySelector('.keep_reservation_check_contents');
+const $check_small = document.querySelector('#check_small');
+const $check_medium = document.querySelector('#check_medium');
+const $check_large = document.querySelector('#check_large');
+const $check_price = document.querySelector('#check_price');
 
 function minus(a) {
     if (a.parentNode.children[1].value > 0) {
@@ -51,14 +63,14 @@ function openSelectLocation() {
 
 function searchAddress() {
     new daum.Postcode({
-        oncomplete: function(data) {
+        oncomplete: function (data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
             $arrive.value = data.address;
         }
     }).open();
 }
 
-async function  deliverySubmit() {
+function deliverySubmit() {
     const arr = [$date, $start, $arrive, $detail_adr, $name, $phone];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].value === '') {
@@ -68,49 +80,66 @@ async function  deliverySubmit() {
             //     title: "알림",
             //     text: `${arr[i].name}을(를) 입력해주세요!`,
             // });
-            window.scrollTo({top:arr[i].offsetTop, behavior: 'smooth'});
+            window.scrollTo({top: arr[i].offsetTop, behavior: 'smooth'});
             return;
         }
     }
 
     if (agree.checked === false) {
         alert('이용약관을 확인해주세요.');
-        window.scrollTo({top:agree.offsetTop, behavior: 'smooth'});
+        window.scrollTo({top: agree.offsetTop, behavior: 'smooth'});
         return;
     }
 
     if (Number($totalPrice.innerText) === 0) {
         alert('짐 개수를 선택해주세요.');
-    }
-    else {
-        const res = await supabase.from('delivery').insert([
-            {name: $name.value, phone: $phone.value, delivery_date: $date.value, delivery_start: $start.value, delivery_arrive: $arrive.value, detail_adr: $detail_adr.value, small: small.value, medium: medium.value, large: large.value, price: Number($totalPrice.innerText)}
-        ]).select();
-
-        if (res.status === 409) {
-            alert('사용중인 전화번호입니다.');
-            window.scrollTo({top:$phone.offsetTop, behavior: 'smooth'});
-            return;
+    } else {
+        const brr = [$check_date, $check_start, $check_arrive, $check_detail_adr, $check_name, $check_phone];
+        for (let i = 0; i < brr.length; i++) {
+            brr[i].innerHTML = arr[i].value;
         }
 
-        if (res.status === 201) {
-            await Swal.fire({
-                title: "배송예약이 완료되었습니다!",
-                icon: "success",
-                draggable: true
-            })
-        }
+        $check_small.innerHTML = small.value;
+        $check_medium.innerHTML = medium.value;
+        $check_large.innerHTML = large.value;
+        $check_price.innerHTML = Number($totalPrice.innerText);
+        $keep_reservation_contents.style.display = 'none';
+        $keep_reservation_check_contents.style.display = 'block';
+
+        window.scrollTo({top: 0, behavior: 'smooth'});
+
     }
-    window.location.href="index.html";
 }
 
-$select_location.addEventListener('click', function (){
-    if(!!document.querySelector('input[name="start_location"]:checked')){
+async function paymentSubmit() {
+    const res = await supabase.from('delivery').insert([
+        {
+            name: $name.value,
+            phone: $phone.value,
+            delivery_date: $date.value,
+            delivery_start: $start.value,
+            delivery_arrive: $arrive.value,
+            detail_adr: $detail_adr.value,
+            small: small.value,
+            medium: medium.value,
+            large: large.value,
+            price: Number($totalPrice.innerText)
+        }
+    ]).select();
+    await Swal.fire({
+        title: "예약이 완료되었습니다!",
+        icon: "success",
+        draggable: true
+    })
+    location.href = 'index.html';
+}
+
+$select_location.addEventListener('click', function () {
+    if (!!document.querySelector('input[name="start_location"]:checked')) {
         $start.value = document.querySelector('input[name="start_location"]:checked').parentNode.children[1].innerText;
 
         closeModal();
-    }
-    else{
+    } else {
         alert("장소를 선택해주세요.")
     }
 });
